aa1d4c1c73d401072663550f84cec4f7c0658c9d
tamzi <3008932+tamzi@users.noreply.github.com>
2025-04-24 23:09:37 +0200

    add all services

--- inventory-service/build.gradle.kts
+++ inventory-service/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "org.tamzi.eltiketi"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 
 java {
     toolchain {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,15 @@
 rootProject.name = "eltiketi"
 
-include("common")
-include("inventoryservice")
-include("admin")
-include("portal")
+// Include subprojects here (e.g., "common", "admin-service", "api-gateway", etc.) for a multi-module mono repo project
+
+listOf(
+    "common",
+    "config-service",
+    "admin-service",
+    "api-gateway",
+    "inventory-service",
+    "booking-service",
+    "order-service",
+    "notification-service"
+).forEach { include(it) }
+
