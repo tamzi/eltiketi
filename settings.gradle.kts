f8d1fa59812ed6dc5078b5d526501fae922f79a7
tamzi <3008932+tamzi@users.noreply.github.com>
2025-04-28 20:03:09 +0200

    add versions toml file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,15 @@
 rootProject.name = "eltiketi"
 
-// Include subprojects here (e.g., "common", "admin-service", "api-gateway", etc.) for a multi-module mono repo project
+// Enable version catalog feature and configure the versions.toml file
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("gradle/versions.toml"))
+        }
+    }
+}
 
+// Include subprojects here (e.g., "common", "admin-service", "api-gateway", etc.) for a multi-module mono repo project
 listOf(
     "common",
     "config-service",
